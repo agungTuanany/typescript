6ea77e95e8b6c07601da062e668e3386d0d54eab
agungTuanany <agung.tuanany@gmail.com>
2019-03-06 10:28:03 +0700

    TypeScript and ES6 --01. Let and Const

---
+++ 03.TypeScript-and-ES6/Readme.md
@@ -0,0 +1,27 @@
+# Objective | 04 TypeScript and ES6
+
+### Videos :
+
+- Block Scope
+- Arrow Functions
+- Arrow Function - Variations
+- The Spread Operator
+- The Rest Operator
+- Destructing Arrays
+- Destructing Objects
+- Template Literals
+- Other ES6 Features
+- Module Exercise Problem
+- Module Exercise Solution
+- Module Summary
+
+
+##### 6-Mar 2019 09:43 -Let and Const
+
+    tsc -w      // to going watch mode
+
+
+
+
+
+
---
+++ 03.TypeScript-and-ES6/ts-files/script.ts
@@ -0,0 +1,41 @@
+// let & const
+console.log(">>> eg: let & const <<<")
+
+/*
+ * The differences between "var" and "let" is the "scope" of variable. The "let"
+ * keyword create a "block-scope" variable which mean it's contain in the block
+ * which you find this variables.
+ *
+ * The "var" variable create a "global-scope" variable. Which can call in
+ * block-scope.
+ *
+ * The "let" keyword is prefer way and a better then "var" keyword since only
+ * has advantages and no disadvantages.
+ */
+
+let variable = "Test";
+console.log(variable);
+variable = "Another Value";
+console.log(variable);
+
+/*
+ * In this upper example we reassign the "let variable" to another value, and
+ * it's possible of course, the only different is the "scope" where "variable"
+ * sits-on, and the end of course you possibly change the value.
+ *
+ * You should keep on your mind the type of variable ("Test") has to equal the
+ * type ("Another Value") had before ("Test"), as automatically.
+ */
+
+const maxLevels = 100;
+console.log(maxLevels);
+// maxLevels = 99;    // Won't work
+
+/*
+ * "const" is immutable data structure or data variable.
+ *
+ * Well it's good practice to use "const" keyword whenever you know the value is
+ * not going to change, because then you make clear in your code and you also
+ * get compilation support and get warning errors if you indeed trying to change
+ * even thought you didn't well want to do so.
+ */
